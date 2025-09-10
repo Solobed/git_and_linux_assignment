27a858cdf0576c75c27430a431ac1fefa75ddc0e
Solobed <134091281+Solobed@users.noreply.github.com>
2025-09-10 23:40:51 +0100

    Readme Update

--- Scripts/bash/posey.sh
+++ Scripts/bash/posey.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Retrieving the database name, user name and input directory
-DB=$1
-USER=$2
-INPUT_DIR=$3
+db=$1
+user=$2
+input_dir=$3
 
 # Iterating over the files
-for file in "$INPUT_DIR"/*.csv; do
+for file in "$input_dir"/*.csv; do
     echo $file
     table_name=$(basename "$file" .csv)
-    psql -U $USER -d $DB -c "\COPY $table_name FROM '$file' WITH CSV HEADER"
+    psql -U $user -d $db -c "\COPY $table_name FROM '$file' WITH CSV HEADER"
     echo "Data inserted sucessfully"
 done
 
